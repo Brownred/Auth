29cc581af9c98fab0356908287a3ec5daf62ddd4
Lenox <lenoxmiheso@gmail.com>
2024-05-06 23:57:29 +0300

    Created the necessary pages and worked on the routes

--- Auth/src/App.tsx
+++ Auth/src/App.tsx
@@ -1,8 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { About, Home, Login, Profile, SignUp } from "./pages/index";
 
 
 export default function App() {
   return (
-    <h1 className="text-red-400">App</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ Auth/src/pages/index.ts
@@ -0,0 +1,7 @@
+import Home from "./home";
+import About from "./about";
+import Login from "./login";
+import SignUp from "./signUp";
+import Profile from "./profile";
+
+export { Home, About, Login, SignUp, Profile };
