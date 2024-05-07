fbadb2c0bf037897f04ec955b0170d581c3c28d6
Lenox <lenoxmiheso@gmail.com>
2024-05-07 23:36:54 +0300

    connected the backend to the database, MongoDB

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,7 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const mongoURI = process.env.MONGO_URI;
+
+if (!mongoURI) {
+    throw new Error('lol :) MONGO_URI is not defined');
+}
+
+mongoose.connect(mongoURI);
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log('serer listening on port 3000');
+    console.log('server listening on port 3000');
 });
