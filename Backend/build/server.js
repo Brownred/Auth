5102fddf90e60c5b2f4927839e19f6b89c37ffce
Lenox <lenoxmiheso@gmail.com>
2024-05-07 23:02:14 +0300

    was setting up the server. Wnated to use typescrpit, but got some issues along the way, but managed at long last

---
+++ Backend/build/server.js
@@ -0,0 +1,5 @@
+import express from 'express';
+const app = express();
+app.listen(3000, () => {
+    console.log('serer listening on port 3000');
+});
---
+++ Backend/src/server.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log('serer listening on port 3000');
+});
