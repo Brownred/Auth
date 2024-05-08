728459d0dd590f1ce98b62da4c5a3d4710b9e191
Lenox <lenoxmiheso@gmail.com>
2024-05-08 23:09:48 +0300

    created a test api route, but was met with an impediment.

---
+++ Backend/src/controllers/user.controller.ts
@@ -0,0 +1,9 @@
+// export const test = (req: Request, res: Response) => {
+//     res.json({ message: 'Hello World!' });
+    
+// };
+
+// export function test (req: Request, res: Response) {
+//     res.json({ message: 'Hello World!' });
+    
+}
--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
+import userRoute from './routes/user.route';
+
 dotenv.config();
 
 const mongoURI = process.env.MONGO_URI;
@@ -10,10 +12,14 @@ if (!mongoURI) {
     throw new Error('lol :) MONGO_URI is not defined');
 }
 
-mongoose.connect(mongoURI).then(() => { console.log('connected to mongodb :)') }).catch((err) => { console.log("opps! :( ", err) });
+mongoose.connect(mongoURI)
+    .then(() => { console.log('connected to mongodb :)') })
+    .catch((err) => { console.log("opps! :( ", err) });
 
 const app = express();
 
+app.use("/api/user", userRoute);
+
 app.listen(3000, () => {
-    console.log('server listening on port 3000');
+    console.log('server listening on http://localhost:3000');
 });
