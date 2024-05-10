3e0725f9e6cbc4fc896ddeebf6a1dc90e2301790
Lenox <lenoxmiheso@gmail.com>
2024-05-10 21:29:33 +0300

    finished working on the login backend logic

--- Backend/src/routes/auth.route.ts
+++ Backend/src/routes/auth.route.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 import User from '../models/user.models';
 import { MongoError } from 'mongodb';
 import { errorHandler } from '../utils/error';
@@ -9,7 +10,7 @@ const authRouter = express.Router();
 
 authRouter.post("/signup", async (req, res, next) => {
     const { username, email, password } = req.body;
-    const hashedPassword = bcryptjs.hashSync(password, 10); 
+    const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({ username, password: hashedPassword, email });
     try {
         await newUser.save();
@@ -20,6 +21,23 @@ authRouter.post("/signup", async (req, res, next) => {
         } else {
             next(errorHandler(500, 'Something went wrong'));
         }
+    }
+});
+
+authRouter.post("/login", async (req, res, next) => {
+    const { username, password } = req.body;
+
+    try {
+        const validUser = await User.findOne({ username });
+        if (!validUser) return next(errorHandler(404, "User not found"));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler(401, "Wrong credentials"));
+        if (!process.env.JWT_SECRET) return next(errorHandler(500, "Internal server error"));
+        const token = jwt.sign({ id: validUser._id?.toString(), name: validUser.username }, process.env.JWT_SECRET, { expiresIn: "2d" });
+        const { password: hashedPassword, __v, ...rest } = validUser;
+        res.cookie("accessToken", token, { httpOnly: true }).status(200).json(rest);
+    } catch (error) {
+        next(error);
     }
 });
 
